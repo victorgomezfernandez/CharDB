09a61aebd6a71dcc4100357c741734bbba1f361d
victorgomezfernandez <victorgomezfernandez@alumno.ieselrincon.es>
2024-11-27 16:05:30 +0000

    creacion proyecto

---
+++ src/main/java/com/charcc/project/charcc/CharCcApplication.java
@@ -0,0 +1,13 @@
+package com.charcc.project.charcc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CharCcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CharCcApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/charcc/project/charcc/CharCcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.charcc.project.charcc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CharCcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
